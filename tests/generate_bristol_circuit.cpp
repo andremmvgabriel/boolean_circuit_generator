7e0838ce87c5fc248625407303e8aef2add7cc3a
André Gabriel <andre.gabriel@altran.com>
2021-08-18 00:25:06 +0100

    Testing XOR, INV, AND, OR, and addition functions.

--- tests/generate_bristol_circuit.cpp
+++ tests/generate_bristol_circuit.cpp
@@ -4,22 +4,33 @@
 #include <Variable.hpp>
 
 int main() {
-    CircuitGenerator circuit_generator(CircuitType::BRISTOL);
+    CircuitGenerator circuit_generator(
+        CircuitType::BRISTOL,
+        {8, 8},
+        {8}
+    );
 
-    Variable input1;
-    Variable input2;
-    Variable output;
+    Variable input1(8);
+    Variable input2(8);
+    Variable output(8);
 
     circuit_generator.add_input(input1);
     circuit_generator.add_input(input2);
 
     circuit_generator.start();
 
-    circuit_generator.add( input1, input2, output );
+    Variable test = circuit_generator.create_constant<uint8_t>(0xF0);
+
+    /* circuit_generator.XOR(output, test, test);
+    circuit_generator.INV(test, test);
+    circuit_generator.AND(output, test, output);
+    circuit_generator.OR(output, test, output); */
+
+    circuit_generator.addition( input1, input2, output );
 
     circuit_generator.conclude();
 
-    circuit_generator.add_output(output);
+    //circuit_generator.add_output(output);
 
     return 0;
 }
