c012fcceaa9c4edce63deb9a803f8bf8f921a073
André Gabriel <andremmvgabriel@gmail.com>
2021-09-06 14:54:20 +0100

    Update compile script

--- compile.bash
+++ compile.bash
@@ -1,6 +1,35 @@
+#!/bin/bash
+
+mkdir -p build
+
 cd build
 
 cmake ..
-cmake --build . --target ALL_BUILD --config Release -- /maxcpucount:12
+
+case "$OSTYPE" in
+	solaris*)
+		echo "> SOLARIS Operating System detected!"
+		echo "   [Warning] Not suported. Returning..."
+	;;
+	darwin*)
+		echo "> OSX Operating System detected!"
+		echo "   [Warning] Not suported. Returning..."
+	;; 
+	linux*)
+		echo "> LINUX Operating System detected!"
+		cmake --build .
+	;;
+	bsd*)
+		echo "> BSD Operating System detected!"
+		echo "   [Warning] Not suported. Returning..."
+	;;
+	msys*)
+		echo "> WINDOWS Operating System detected!" 
+		cmake --build . --target ALL_BUILD --config Release -- /maxcpucount:12
+	;;
+	*)
+		echo "unknown: $OSTYPE"
+	;;
+esac
 
 cd ..
