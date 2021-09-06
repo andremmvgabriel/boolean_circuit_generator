dba2ee9991814cefab47ebf7c217c43dc68061b0
gabekebab <andremmvgabriel@gmail.com>
2021-09-06 22:59:29 +0100

    Fixed wire assignment for multiple parties for Libscapi Generator

--- src/LibscapiCircuitGenerator.cpp
+++ src/LibscapiCircuitGenerator.cpp
@@ -26,16 +26,24 @@ void gabe::circuits::generator::LibscapiGenerator::_write_header() {
         assigned_inputs += _number_wires_input_parties.at(i);
     }
 
-    uint64_t assigned_outputs = 0x00;
+    uint64_t wires_to_assign = 0x00;
+    for (auto & amount : _number_wires_output_parties) { wires_to_assign += amount; }
     std::string outputs;
-    for (int i = 0; i < _number_wires_output_parties.size(); i++) {
-        outputs += std::to_string(i + 1) + " " + std::to_string(_number_wires_output_parties.at(i)) + sbreak;
-        for (uint64_t j = _number_wires_output_parties.at(i); j > 0; j--) {
-            outputs += std::to_string( _counter_wires - j - assigned_outputs) + sbreak;
+    int counter = 1;
+    for (auto & amount : _number_wires_output_parties) {
+        outputs += std::to_string(counter) + " " + std::to_string(amount) + sbreak;
+
+        // Assigns the wires values
+        for (uint64_t i = amount; i > 0; i--) {
+            outputs += std::to_string( _counter_wires - wires_to_assign + amount - i ) + sbreak;
         }
+
         outputs += sbreak;
-        assigned_outputs += _number_wires_output_parties.at(i);
-    }
+
+        wires_to_assign -= amount;
+
+        counter++;
+    } 
 
     // Writes the header
     _circuit_file.write( header.c_str(), header.size() );
